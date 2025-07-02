d7182ddb6ec4bc84f4b089432ac21473bae7e85a
Louis Joly <louisjoly.dev@gmail.com>
2025-07-02 12:17:02 +0200

    feat: add base entity model
    - Create BaseEntity.cs with common properties
    - Implement IBaseEntity interface
    - Add audit fields (CreatedDate, UpdatedDate)

---
+++ BugTracker.Api/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace BugTracker.Api.Models
+{
+    public abstract class BaseEntity
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("updatedAt")]
+        [JsonInclude]
+        public DateTime? UpdatedAt { get; set; }
+
+        [JsonPropertyName("isDeleted")]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
